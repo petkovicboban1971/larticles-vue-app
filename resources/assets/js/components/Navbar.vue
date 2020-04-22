27190c4c6cbbacebf6e50a744650f856e78c2069
bexterDesign <office@bexter.rs>
2020-04-22 09:27:30 +0200

    first_commit

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="nav navbar-expand-sm navbar-dark bg-info mb-2">
+    <div class="container">
+      <a href="/" class="navbar-brand">Link1</a>
+      <a href="#" class="navbar-brand">Link2</a>
+      <a href="#" class="navbar-brand">Link3</a>
+    </div>
+  </nav>
+</template>
+<style scoped>
+	.nav{
+		padding:5px;
+	}
+	.navbar-brand {
+		font-size: 16pt;
+		font-family: Courier New;
+	}
+</style>
